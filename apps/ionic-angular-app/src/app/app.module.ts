483cac64bfd5e18b6fd97f1bd67f1b73767645dd
Devin Shoemaker <devinshoe@gmail.com>
2019-11-08 12:02:47 -0600

    Add routes to home module and component

--- apps/ionic-angular-app/src/app/app.module.ts
+++ apps/ionic-angular-app/src/app/app.module.ts
@@ -1,15 +1,22 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-import { AppComponent } from './app.component';
-import { RouterModule } from '@angular/router';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
 import { IonicModule } from '@ionic/angular';
+import { AppComponent } from './app.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  {
+    path: 'home',
+    loadChildren: () => import('./home/home.module').then(m => m.HomeModule)
+  }
+];
 
 @NgModule({
   declarations: [AppComponent],
   imports: [
     BrowserModule,
-    RouterModule.forRoot([], { initialNavigation: 'enabled' }),
+    RouterModule.forRoot(routes, { initialNavigation: 'enabled' }),
     IonicModule.forRoot()
   ],
   providers: [],
